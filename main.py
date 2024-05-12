4addfa7639d215a6cbc945d8c77ae521e1aa1b82
Tesla2000 <fratajczak124@gmail.com>
2024-05-12 12:35:50 +0200

    Added requirements.txt and updated main.py

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-from collections import deque
-from copy import deepcopy
-from itertools import count
+import random
 
-from Config import Config
-from agent.Agent import Agent
-from agent.load_agents import load_agents
-from agent.save_agent import save_agent
-from agent.self_play import self_play
-from agent.train_agent import train_agent
-from db.load_train_buffer import load_train_buffer
-from db.save_train_buffer import save_train_buffer
-from training_variants.train_to_go_fast import train_to_go_fast
+from src.Game import Game
 
 
 def main():
-    # train_loop()
-    train_to_go_fast()
+    game = Game()
+    while not game.is_terminal():
+        actions = game.get_possible_actions()
+        action = random.choice(actions)
+        game = game.perform(action)
 
 
 if __name__ == "__main__":
