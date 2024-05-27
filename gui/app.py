32fc8afae45d1f42ec4cd458cf483fd17841e0bf
Tesla2000 <fratajczak124@gmail.com>
2024-05-27 22:35:56 +0200

    Added click method back

--- gui/app.py
+++ gui/app.py
@@ -1,10 +1,13 @@
-from dataclasses import asdict
+from dataclasses import asdict, astuple
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
 
+from perform_move import perform_move
 from src.Game import Game
+from src.entities.AllResources import AllResources
 from src.entities.BasicResources import BasicResources
-from src.entities.Card import empty_card, Card
+from src.entities.Card import empty_card
+from src.moves import GrabThreeResource, GrabTwoResource
 
 app = Flask(__name__)
 
@@ -20,16 +23,50 @@ def select_aristocrats(game: Game):
 
 game = Game()
 
+image2build = {  # to be wired
+    "image9": game.all_moves[23],
+    "image10": game.all_moves[24],
+    "image11": game.all_moves[25],
+    "image12": game.all_moves[26],
+    "image36": game.all_moves[29],
+    "image14": game.all_moves[19],
+    "image15": game.all_moves[20],
+    "image16": game.all_moves[21],
+    "image17": game.all_moves[22],
+    "image37": game.all_moves[28],
+    "image19": game.all_moves[15],
+    "image20": game.all_moves[16],
+    "image21": game.all_moves[17],
+    "image22": game.all_moves[18],
+    "image38": game.all_moves[27],
+}
 
-def card_to_dict(card: Card):
-    prod2str = {
-        BasicResources(red=1): "RED",
-        BasicResources(green=1): "GREEN",
-        BasicResources(blue=1): "BLUE",
-        BasicResources(black=1): "BLACK",
-        BasicResources(white=1): "WHITE",
-    }
-    return dict(**asdict(card.cost), production=prod2str[card.production])
+image2reserve = {  # to be wired
+    "image9": game.all_moves[38],
+    "image10": game.all_moves[39],
+    "image11": game.all_moves[40],
+    "image12": game.all_moves[41],
+    "image13": game.all_moves[44],
+    "image14": game.all_moves[34],
+    "image15": game.all_moves[35],
+    "image16": game.all_moves[36],
+    "image17": game.all_moves[37],
+    "image18": game.all_moves[43],
+    "image19": game.all_moves[30],
+    "image20": game.all_moves[31],
+    "image21": game.all_moves[32],
+    "image22": game.all_moves[33],
+    "image23": game.all_moves[42],
+}
+
+image2resource = {
+    "resource_red": BasicResources(red=1),
+    "resource_green": BasicResources(green=1),
+    "resource_blue": BasicResources(blue=1),
+    "resource_black": BasicResources(black=1),
+    "resource_white": BasicResources(white=1),
+}
+grabbed_resources = BasicResources()
 
 
 @app.route("/")
@@ -53,6 +90,47 @@ def index():
                                           game.current_player.reserve)),
         aristocrats_urls=aristocrats_urls,
     )
+
+
+@app.route('/click', methods=['POST'])
+def click():
+    global grabbed_resources, game
+    data = request.json
+    image_class = data.get('class')
+    right_click = data.get('clickType') == "right"
+    if image_class in image2build:
+        if right_click:
+            action = image2reserve[image_class]
+        else:
+            action = image2build[image_class]
+        if not action.is_valid(game) or astuple(grabbed_resources):
+            return jsonify(success=False)
+        game = perform_move(game, action)
+        return jsonify(success=True, turn_finished=True)
+    if image_class in image2resource:
+        chosen_resource = image2resource[image_class]
+        if right_click:
+            new_resources = AllResources(*grabbed_resources) - chosen_resource
+            if new_resources.lacks():
+                return jsonify(success=False)
+            grabbed_resources = new_resources.get_basic()
+        else:
+            grabbed_resources += chosen_resource
+        if sum(grabbed_resources) == 3:
+            game = perform_move(game, GrabThreeResource(grabbed_resources))
+            return jsonify(success=True, turn_finished=True)
+        if max(grabbed_resources) == 2 and game.board.resources[
+            astuple(grabbed_resources).index(max(grabbed_resources))
+        ] < 2:
+            return jsonify(success=False)
+        if max(grabbed_resources) == 2 and sum(grabbed_resources) == 3:
+            return jsonify(success=False)
+        if max(grabbed_resources) == 2 and sum(grabbed_resources) != 3:
+            game = perform_move(game, GrabTwoResource(grabbed_resources))
+            return jsonify(success=True, turn_finished=True)
+        return jsonify(success=True, turn_finished=False)
+
+    return jsonify(success=False)
 
 
 if __name__ == "__main__":
